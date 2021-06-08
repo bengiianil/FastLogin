c3a69a850f0f2da277bcd61bfaa4b9f02115c160
BENGI ANIL <TCBANIL@turkcell.entp.tgc>
2021-06-08 15:47:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fastlogin/DGLoginCoordinator.java
@@ -0,0 +1,5 @@
+package com.example.fastlogin;
+
+public class DGLoginCoordinator {
+
+}
---
+++ app/src/main/java/com/example/fastlogin/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.fastlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+
+}
