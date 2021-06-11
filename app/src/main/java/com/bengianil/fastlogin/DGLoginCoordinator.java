73741913d1305c4585b60be4c02ad583002ffe72
bengiianil <bengianil34@gmail.com>
2021-06-11 15:00:40 +0300

    DGLoginCoordinator added

--- app/src/main/java/com/bengianil/fastlogin/DGLoginCoordinator.java
+++ app/src/main/java/com/bengianil/fastlogin/DGLoginCoordinator.java
@@ -1,6 +1,100 @@
 package com.bengianil.fastlogin;
 
-class DGLoginCoordinator private constructor(builder: DGLoginCoordinator.Builder) {
+import java.net.URI;
 
+public class DGLoginCoordinator {
+
+    /** Property **/
+    Integer appID;
+    String language;
+    URI serviceRedirectUrl;
+    Boolean disableCellLogin;
+    Boolean autoLoginOnly;
+    Boolean disableAutoLogin;
+    String state;
+
+    /** Constructor **/
+    public DGLoginCoordinator(Builder builder) {
+        this.appID = builder.appID;
+        this.language = builder.language;
+        this.serviceRedirectUrl = builder.serviceRedirectUrl;
+        this.disableCellLogin = builder.disableCellLogin;
+        this.autoLoginOnly = builder.autoLoginOnly;
+        this.disableAutoLogin = builder.disableAutoLogin;
+        this.state = builder.state;
+    }
+
+
+    /** Getter Methods **/
+    public Integer getAppID() {
+        return appID;
+    }
+    public String getLanguage() {
+        return language;
+    }
+    public URI getServiceRedirectUrl() {
+        return serviceRedirectUrl;
+    }
+
+    public Boolean getDisableCellLogin() {
+        return disableCellLogin;
+    }
+    public Boolean getAutoLoginOnly() {
+        return autoLoginOnly;
+    }
+    public Boolean getDisableAutoLogin() {
+        return disableAutoLogin;
+    }
+    public String getState() {
+        return state;
+    }
+
+
+    /** Builder Pattern **/
+    public static class Builder{
+
+        private Integer appID;
+        private String language;
+        private URI serviceRedirectUrl;
+        private Boolean disableCellLogin;
+        private Boolean autoLoginOnly;
+        private Boolean disableAutoLogin;
+        private String state;
+
+        public Builder(){ }
+
+        public Builder appID(Integer appID){
+            this.appID = appID;
+            return this;
+        }
+        public Builder language(String language){
+            this.language = language;
+            return this;
+        }
+        public Builder serviceRedirectUrl(URI serviceRedirectUrl){
+            this.serviceRedirectUrl = serviceRedirectUrl;
+            return this;
+        }
+        public Builder disableCellLogin(Boolean disableCellLogin){
+            this.disableCellLogin = disableCellLogin;
+            return this;
+        }
+        public Builder autoLoginOnly(Boolean autoLoginOnly){
+            this.autoLoginOnly = autoLoginOnly;
+            return this;
+        }
+        public Builder disableAutoLogin(Boolean disableAutoLogin){
+            this.disableAutoLogin = disableAutoLogin;
+            return this;
+        }
+        public Builder state(String state){
+            this.state = state;
+            return this;
+        }
+
+        public DGLoginCoordinator build() {
+            return new DGLoginCoordinator(this);
+        }
+
+    }
 }
-
--- app/src/main/java/com/bengianil/fastlogin/MainActivity.java
+++ app/src/main/java/com/bengianil/fastlogin/MainActivity.java
@@ -4,13 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.net.URI;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        URI serviceURL = URI.create("https://bireysel.turkcell.com.tr/fastloginResult");
+        DGLoginCoordinator dg = new DGLoginCoordinator.Builder()
+                .appID(12345)
+                .language("TR")
+                .serviceRedirectUrl(serviceURL)
+                .disableCellLogin(false)
+                .autoLoginOnly(false)
+                .disableAutoLogin(false)
+                .state("123456")
+                .build();
+
+
     }
 
 }
